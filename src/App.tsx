dda8485516f298d13e674e512c8d2c032c3a959d
Guilherme Muniz <gui.munizalemao@gmail.com>
2025-06-25 13:24:03 -0300

    build: add development dependency

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-red-700 font-bold">Budget Manager</h1>
+      <p>Welcome to the Budget Manager application!</p>
+      <p>This is a simple app to manage your budget.</p>
+      <p>Feel free to explore the features and functionalities.</p>
+      <p>Happy budgeting!</p>
+    </>
+  );
+}
+
+export default App;
