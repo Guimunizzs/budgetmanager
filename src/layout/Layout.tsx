07ee4ae3b25417bd9b9fb3f9b97f159d19415b5d
Guilherme Muniz <gui.munizalemao@gmail.com>
2025-06-27 10:59:15 -0300

    feat: add component header

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const Layout = () => {
   return (
     <div>
-      <header>
-        <h1>Budget Manager</h1>
-        {/* Aqui você pode adicionar navegação, menu, etc. */}
-      </header>
+      <Header />
       <main>
         <Outlet />
       </main>
