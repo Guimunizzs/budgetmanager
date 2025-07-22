f9f1713c867252b0d8842a2f8c6c594a08c3af3b
Guilherme Muniz <gui.munizalemao@gmail.com>
2025-07-22 10:23:08 -0300

    fix: fix Router.tsx

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,15 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
 import Budget from "../pages/Budget";
-import Layout from "../layout/Layout";
 import Transactions from "../pages/Transactions";
 import EditTransaction from "../pages/EditTransaction";
+import ProtectedRoute from "./ProtectedRoute";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Layout />}>
+
+      <Route path="/" element={<ProtectedRoute />}>
+        <Route index element={<Dashboard />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="transactions" element={<Transactions />} />
         <Route path="transactions/edit/:id" element={<EditTransaction />} />
