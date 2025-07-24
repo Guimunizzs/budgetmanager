b68262b6c1b868b56c9c3e1a59fb2ea439d6a757
Guilherme Muniz <gui.munizalemao@gmail.com>
2025-07-24 10:07:38 -0300

    .

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// Cole a configuração do seu projeto Firebase aqui
 const firebaseConfig = {
-  apiKey: "AIzaSy..._SUA_CHAVE_REAL_...",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456",
+  apiKey: "AIzaSyA9s3TTgY1jDZihF_nJ1-0zhF4EuWW1bMQ",
+  authDomain: "budgetmanager-31d09.firebaseapp.com",
+  projectId: "budgetmanager-31d09",
+  storageBucket: "budgetmanager-31d09.firebasestorage.app",
+  messagingSenderId: "595047601522",
+  appId: "1:595047601522:web:bcc84dcb844c96b99239eb",
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
-
 export { app, auth, googleProvider };
